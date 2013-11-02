fad0f193b391cd7e5654f047aa30e9fb0839dfae
Juan Jardim <juanjardim@gmail.com>
2013-11-02 07:25:17 +0000

    My web site versio 1.0

---
+++ application/Bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+
+    protected function _initNavigation() {
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+        $config = new Zend_Config_Xml(APPLICATION_PATH . '/configs/navigation.xml');
+
+        $navigation = new Zend_Navigation($config);
+        $view->navigation($navigation);
+    }
+
+}
+
---
+++ application/controllers/WorkController.php
@@ -0,0 +1,55 @@
+<?php
+
+class WorkController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function publicationsAction()
+    {
+        // action body
+    }
+
+    public function thesisAction()
+    {
+        // action body
+    }
+
+    public function metavisualizerAction()
+    {
+        // action body
+    }
+
+    public function autochefAction()
+    {
+        // action body
+    }
+
+    public function herbalclinicAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
